022c82df7257fc0d9265145365f479e64597a8d8
Muhamad Wahyudin <muhammad.whydn@gmail.com>
2023-09-09 20:50:55 +0700

    feature: add domain layer DI module that's scoped into viewmodel

--- app/src/main/java/dev/hyuwah/imusic/features/search/di/SearchModule.kt
+++ app/src/main/java/dev/hyuwah/imusic/features/search/di/SearchModule.kt
@@ -1,11 +1,18 @@
 package dev.hyuwah.imusic.features.search.di
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 import dev.hyuwah.imusic.core.common.di.CommonProviderModule
 import dev.hyuwah.imusic.features.search.data.remote.response.ITunesSearchService
+import dev.hyuwah.imusic.features.search.data.repository.SearchRepositoryImpl
+import dev.hyuwah.imusic.features.search.domain.repository.SearchRepository
+import dev.hyuwah.imusic.features.search.domain.usecase.SearchMusicUseCase
+import dev.hyuwah.imusic.features.search.domain.usecase.SearchMusicUseCaseImpl
 import retrofit2.Retrofit
 import javax.inject.Named
 
@@ -20,4 +27,17 @@ object SearchProviderModule {
         return retrofit.create(ITunesSearchService::class.java)
     }
 
+}
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class SearchBinderModule {
+
+    @Binds
+    @ViewModelScoped
+    abstract fun bindSearchRepository(impl: SearchRepositoryImpl): SearchRepository
+
+    @Binds
+    @ViewModelScoped
+    abstract fun bindSearchMusicUseCase(impl: SearchMusicUseCaseImpl): SearchMusicUseCase
 }
